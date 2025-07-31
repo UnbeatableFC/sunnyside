f68c5b595d5fcd21299fd9b8eae706b5c00c5b77
Wisdom Mpamugo <wisdommpam99@gmail.com>
2025-07-31 14:56:25 +0100

    Final

--- src/components/tabs/Testimonials.tsx
+++ src/components/tabs/Testimonials.tsx
@@ -4,12 +4,12 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 export function Testimonials() {
   return (
-    <div className="px-25 py-36">
-      <div className="text-center fraunces-700 tracking-[8px] text-xl font-bold text-muted-foreground/50">
+    <div className="md:px-25 py-36">
+      <div className="text-center items-center justify-center fraunces-700 tracking-[8px] text-xl font-bold text-muted-foreground/50">
         CLIENT TESTIMONIALS
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-evenly gap-6 md:gap-3 sm:-mx-0 -mx-18 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-10 md:mx-0 justify-evenly gap-6 md:gap-3 sm:-mx-0 mt-8">
         {testimonials.map(({ name, image, position, sayings }) => (
           <Card className="justify-center items-center gap-y-10">
             <Avatar className="size-12">
@@ -26,7 +26,9 @@ export function Testimonials() {
             </CardContent>
 
             <CardFooter className="flex flex-col">
-              <p className="text-lg font-black fraunces-900">{name}</p>
+              <p className="text-lg font-black fraunces-900">
+                {name}
+              </p>
               <p className="text-xs barlow-semibold text-gray-600/40">
                 {position}
               </p>
